5fe985985952a925b9daad6764245fb01f176539
Yannis <yannis@lystable.com>
2016-02-15 18:07:54 +0000

    "(dennis) Initial Release Commit"

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name='GitflowDennis',
-    version='0.10.0',
+    version='0.11.0',
     description='Dennis the release helper',
     packages=['dennis'],
     long_description=README,
